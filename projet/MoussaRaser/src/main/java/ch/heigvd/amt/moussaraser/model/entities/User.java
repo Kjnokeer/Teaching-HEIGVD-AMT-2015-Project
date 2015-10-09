92ae8529430ec55e4cd6eda0e9f6b07e466d27d4
Mathias Dolt <mathias.dolt@heig-vd.ch>
2015-10-09 10:23:41 +0200

    refractor Users.java to User.java

---
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/model/entities/User.java
@@ -0,0 +1,84 @@
+package ch.heigvd.amt.moussaraser.model.entities;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+@Entity
+@NamedQueries({
+  @NamedQuery(name = "Users.findByNameAndPassword", query = "SELECT u FROM Users u WHERE u.firstName = :fname AND u.password = :pass"),
+})
+
+public class User extends AbstractDomainModelEntity<Long> {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+
+  public User() {
+  }
+
+   /**
+    * @return the firstName
+    */
+   public String getFirstName() {
+      return firstName;
+   }
+
+   /**
+    * @param firstName the firstName to set
+    */
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   /**
+    * @return the lastName
+    */
+   public String getLastName() {
+      return lastName;
+   }
+
+   /**
+    * @param lastName the lastName to set
+    */
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   /**
+    * @return the email
+    */
+   public String getEmail() {
+      return email;
+   }
+
+   /**
+    * @param email the email to set
+    */
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   /**
+    * @return the password
+    */
+   public String getPassword() {
+      return password;
+   }
+
+   /**
+    * @param password the password to set
+    */
+   public void setPassword(String password) {
+      this.password = password;
+   }
+  
+}
