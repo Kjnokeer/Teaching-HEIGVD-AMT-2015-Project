dda3f15b040d6adb0ae3589097d70225db88c899
Manamiz <thibduch@hotmail.com>
2015-10-09 10:28:57 +0200

    Ajout userDAO

---
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/services/dao/UsersDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.amt.moussaraser.services.dao;
+
+import ch.heigvd.amt.moussaraser.model.entities.User;
+import java.util.List;
+
+/**
+ *
+ * @author thibaud
+ */
+public class UsersDAO extends GenericDAO<User, Long> implements UsersDAOLocal {
+
+
+}
---
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/services/dao/UsersDAOLocal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.amt.moussaraser.services.dao;
+
+import ch.heigvd.amt.moussaraser.model.entities.User;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author thibaud
+ */
+@Local
+public interface UsersDAOLocal extends IGenericDAO<User, Long> {
+    
+}
