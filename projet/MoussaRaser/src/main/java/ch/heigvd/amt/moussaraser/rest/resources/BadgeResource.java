c3252dfc8cacce4cec8c069a1db22286f4ad2d23
Mathias Dolt <mathias.dolt@heig-vd.ch>
2015-11-27 10:34:09 +0100

    API REST badges second

--- projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/resources/BadgeResource.java
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/resources/BadgeResource.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.amt.moussaraser.rest.resources;
 
 import ch.heigvd.amt.moussaraser.model.entities.Badge;
-import java.util.ArrayList;
+import ch.heigvd.amt.moussaraser.services.dao.BadgeDAOLocal;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -15,35 +10,23 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-/**
- *
- * @author Olivier Liechti (olivier.liechti@heig-vd.ch)
- */
 @Stateless
 @Path("/badges")
 public class BadgeResource {
 
-    @EJB
-    BadgeDAOLocal badgeDAO;
+   @EJB
+   BadgeDAOLocal badgeDAO;
 
-    @GET
-    @Produces("application/json")
-    public List<Badge> getUsers() {
-        List<Badge> endUsers = endUsersDAO.findAll();
-        List<EndUserDTO> endUsersDTO = new ArrayList<>();
+   @GET
+   @Produces("application/json")
+   public List<Badge> getBadges() {
+      return badgeDAO.findAll();
+   }
 
-        for (EndUser endUser : endUsers) {
-            endUsersDTO.add(new EndUserDTO(endUser.getFirstName(), endUser.getLastName()));
-        }
-
-        return endUsersDTO;
-    }
-
-    @GET
-    @Path("/{id}")
-    @Produces("application/json")
-    public EndUserDTO getUser(@PathParam("id") long id) {
-        EndUser endUser = endUsersDAO.findById(id);
-        return new EndUserDTO(endUser.getFirstName(), endUser.getLastName());
-    }
+   @GET
+   @Path("/{id}")
+   @Produces("application/json")
+   public Badge getBadge(@PathParam("id") long id) {
+      return badgeDAO.findById(id);
+   }
 }
