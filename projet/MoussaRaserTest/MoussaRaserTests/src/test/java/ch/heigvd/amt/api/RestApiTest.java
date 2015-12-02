e517694a5d19dd8b60c9680a8ee5edfaae2a1e3d
Mario Ferreira <mario.jorge.ferreira@gmail.com>
2015-12-02 16:01:38 +0100

    RestApi test - create end user

--- projet/MoussaRaserTest/MoussaRaserTests/src/test/java/ch/heigvd/amt/api/RestApiTest.java
+++ projet/MoussaRaserTest/MoussaRaserTests/src/test/java/ch/heigvd/amt/api/RestApiTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.probedock.client.annotations.ProbeTest;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -32,8 +33,7 @@ public class RestApiTest {
         client = ClientBuilder.newClient();
         mapper = new ObjectMapper();
         factory = new JsonNodeFactory(false);
-    }
-    
+    }    
     
     // Test Get Users
     @Test
@@ -143,5 +143,46 @@ public class RestApiTest {
         JsonNode[] rootNodeasArray = mapper.readValue(jsonPayload, JsonNode[].class);
         assertThat(rootNodeasArray).isNotNull();
         assertThat(rootNodeasArray).isEmpty();
+    }
+    
+    @Test
+    @ProbeTest(tags = "REST")
+    public void itShouldBePossibleToCreateAnEndUser() throws IOException {
+        String apiKeyWithUsers = "092abcde6ec143c0bbe132253263e340";
+        /**
+        * Generate a random sector name, so that we know for sure that there will
+        * be no conflict when creating the new sector
+        */
+       String firstName = "Thibaud";
+       String lastName = "Ducoud";
+       JsonNode userInfo = factory.objectNode().put("firstname", firstName)
+            .put("lastname", lastName);
+       
+       System.out.println(userInfo);
+       
+
+       /**
+        * Send the POST request with the JSON payload to create a new Sector
+        */
+       WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", apiKeyWithUsers);;
+       Response response = target.request().post(Entity.entity(userInfo, "application/json"));
+
+       /**
+        * Check that we have received a 201 (CREATED) status code and that the
+        * Location header is there.
+        */
+       assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+       assertThat(response.getHeaderString("Location")).isNotNull();
+
+       /**
+        * Send a GET request to retrieve the entity that was just created. If the
+        * Location header is correct and if the resource has been created, we can
+        * validate its state compared to what we have sent before.
+        */
+       target = client.target(response.getHeaderString("Location"));
+       response = target.request().get();
+       String jsonPayload = response.readEntity(String.class);
+       JsonNode root = mapper.readTree(jsonPayload);
+       assertThat(root.get("name").asText()).isEqualTo(userInfo);
     }
 }
