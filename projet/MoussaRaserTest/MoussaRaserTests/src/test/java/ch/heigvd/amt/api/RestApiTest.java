47a49ced8162bc45b2c13cafa7a3f808932ec204
Mario Ferreira <mario.jorge.ferreira@gmail.com>
2015-12-02 17:56:03 +0100

    RestApi test - modify end user

--- projet/MoussaRaserTest/MoussaRaserTests/src/test/java/ch/heigvd/amt/api/RestApiTest.java
+++ projet/MoussaRaserTest/MoussaRaserTests/src/test/java/ch/heigvd/amt/api/RestApiTest.java
@@ -20,159 +20,219 @@ import org.junit.Before;
 public class RestApiTest {
 
     private final String baseUrl = "http://localhost:8080/MoussaRaser/api";
+    // Clé api avec le bon format mais qui n'existe pas dans la base de données
+    private final String fakeApiKey = "092abcde6ec143c0bbe132253263e34s";
+    // Clé api d'une application existante sans utilisateurs finaux
+    private final String apiKeyWithUsers = "092abcde6ec143c0bbe132253263e340";
+    // Clé api d'une application existante avec utilisateurs finaux
+    private final String apiKeyWithoutUsers = "8def28edc4504a93b1acf88475f4d707";
 
-    Client client;
-
-    ObjectMapper mapper;
-
-    JsonNodeFactory factory;
+    private Client client;
+    private ObjectMapper mapper;
+    private JsonNodeFactory factory;
 
     @Before
     public void setup() {
         client = ClientBuilder.newClient();
         mapper = new ObjectMapper();
         factory = new JsonNodeFactory(false);
-    }    
-    
+    }
+
     // Test Get Users
     @Test
     @ProbeTest(tags = "REST")
     public void sendingGetUsersWithoutApiKeyReturn401() throws IOException {
         String emptyApiKey = "";
         String messageError = "No API key was provided with the API request";
+
         /**
-         * Send the POST request with the JSON payload to create a new Sector
+         * Requête GET à l'api pour récupérer liste d'utilisateurs d'une
+         * application
          */
         WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", emptyApiKey);
-
         Response response = target.request().get();
 
+        /**
+         * L'api doit renvoyer un code 401
+         */
         assertThat(response.getStatus()).isEqualTo(Response.Status.UNAUTHORIZED.getStatusCode());
 
+        /**
+         * Récupérer la réponse et contrôler qu'elle n'est pas vide
+         */
         String jsonPayload = response.readEntity(String.class);
         assertThat(jsonPayload).isNotNull();
         assertThat(jsonPayload).isNotEmpty();
-        
-        System.out.println(jsonPayload);
-        
+
+        /**
+         * L'api doit renvoyer le bon code d'erreur
+         */
         JsonNode root = mapper.readTree(jsonPayload);
         assertThat(root.get("error").asText().compareToIgnoreCase(messageError));
     }
-    
+
     @Test
     @ProbeTest(tags = "REST")
     public void sendingGetUsersWithFakeApiKeyReturn401() throws IOException {
-        String fakeApiKey = "092abcde6ec143c0bbe132253263e34s";
         String messageError = "An invalid API key was provided with the API request";
         /**
-         * Send the POST request with the JSON payload to create a new Sector
+         * Requête GET à l'api pour récupérer liste d'utilisateurs d'une
+         * application
          */
         WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", fakeApiKey);
-
         Response response = target.request().get();
 
+        /**
+         * L'api doit renvoyer un code 401
+         */
         assertThat(response.getStatus()).isEqualTo(Response.Status.UNAUTHORIZED.getStatusCode());
 
+        /**
+         * Récupérer la réponse et contrôler qu'elle n'est pas vide
+         */
         String jsonPayload = response.readEntity(String.class);
         assertThat(jsonPayload).isNotNull();
         assertThat(jsonPayload).isNotEmpty();
-        
-        System.out.println(jsonPayload);
 
+        /**
+         * L'api doit renvoyer le bon code d'erreur
+         */
         JsonNode root = mapper.readTree(jsonPayload);
         assertThat(root.get("error").asText().compareToIgnoreCase(messageError));
     }
-    
+
     @Test
     @ProbeTest(tags = "REST")
     public void sendingGetUsersToExistingApiKeyReturnOk() throws IOException {
-        String apiKeyWithUsers = "092abcde6ec143c0bbe132253263e340";
         /**
-         * Send the POST request with the JSON payload to create a new Sector
+         * Requête GET à l'api pour récupérer liste d'utilisateurs d'une
+         * application
          */
         WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", apiKeyWithUsers);
-
         Response response = target.request().get();
 
+        /**
+         * L'api doit renvoyer un code 200
+         */
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
 
+        /**
+         * Récupérer la réponse et contrôler qu'elle n'est pas vide
+         */
         String jsonPayload = response.readEntity(String.class);
         assertThat(jsonPayload).isNotNull();
         assertThat(jsonPayload).isNotEmpty();
 
+        /**
+         * Formater au format JSON
+         */
         JsonNode[] rootNodeasArray = mapper.readValue(jsonPayload, JsonNode[].class);
         assertThat(rootNodeasArray).isNotNull();
         assertThat(rootNodeasArray).isNotEmpty();
 
+        /**
+         * L'api doit renvoyer les informations suivantes
+         */
         for (JsonNode users : rootNodeasArray) {
             assertThat(users.get("firstname")).isNotNull();
             assertThat(users.get("lastname")).isNotNull();
         }
     }
-    
+
     @Test
     @ProbeTest(tags = "REST")
     public void sendingGetUsersEmptyToExistingApiKeyReturnOkEmpty() throws IOException {
-        String apiKeyWithoutUsers = "8def28edc4504a93b1acf88475f4d707";
         /**
-         * Send the POST request with the JSON payload to create a new Sector
+         * Requête GET à l'api pour récupérer liste d'utilisateurs d'une
+         * application
          */
         WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", apiKeyWithoutUsers);
-
         Response response = target.request().get();
 
+        /**
+         * L'api doit renvoyer un code 200
+         */
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
 
+        /**
+         * Récupérer la réponse et contrôler qu'elle n'est pas vide
+         */
         String jsonPayload = response.readEntity(String.class);
         assertThat(jsonPayload).isNotNull();
         assertThat(jsonPayload).isNotEmpty();
 
-        System.out.println(jsonPayload);
-
+        /**
+         * Formater au format JSON
+         */
         JsonNode[] rootNodeasArray = mapper.readValue(jsonPayload, JsonNode[].class);
+        /**
+         * L'api doit renvoyer une valeur non null, mais vide
+         */
         assertThat(rootNodeasArray).isNotNull();
         assertThat(rootNodeasArray).isEmpty();
+    }
+
+    @Test
+    @ProbeTest(tags = "REST")
+    public void itShouldBePossibleToCreateAnEndUser() throws IOException {
+        /**
+         * Création de l'utilisateur au format JSON
+         */
+        String firstName = "Thibaud";
+        String lastName = "Duchoud";
+        JsonNode userInfo = factory.objectNode().put("firstName", firstName)
+                .put("lastName", lastName);
+
+        /**
+         * Requête POST à l'api pour ajouter l'utilisateur créé auparavant à
+         * l'application
+         */
+        WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", apiKeyWithUsers);
+        Response response = target.request().post(Entity.entity(userInfo, "application/json"));
+
+        /**
+         * L'api doit renvoyer un code 201
+         */
+        assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+
+        /**
+         * L'api doit renvoyer les informations de l'utilisateur correctes
+         */
+        String jsonPayload = response.readEntity(String.class);
+        JsonNode root = mapper.readTree(jsonPayload);
+        assertThat(root.get("firstname").asText().compareToIgnoreCase(firstName));
+        assertThat(root.get("lastname").asText().compareToIgnoreCase(lastName));
     }
     
     @Test
     @ProbeTest(tags = "REST")
-    public void itShouldBePossibleToCreateAnEndUser() throws IOException {
-        String apiKeyWithUsers = "092abcde6ec143c0bbe132253263e340";
+    public void itShouldBePossibleToModifyAnEndUser() throws IOException {
         /**
-        * Generate a random sector name, so that we know for sure that there will
-        * be no conflict when creating the new sector
-        */
-       String firstName = "Thibaud";
-       String lastName = "Duchoud";
-       JsonNode userInfo = factory.objectNode().put("firstName", firstName)
-            .put("lastName", lastName);
-       
-       System.out.println(userInfo);
-       
+         * Création de l'utilisateur au format JSON
+         */
+        String firstName = "Mario";
+        String lastName = "Modif";
+        JsonNode userInfo = factory.objectNode().put("firstName", firstName)
+                .put("lastName", lastName);
 
-       /**
-        * Send the POST request with the JSON payload to create a new Sector
-        */
-       WebTarget target = client.target(baseUrl).path("users").queryParam("apiKey", apiKeyWithUsers);
-       Response response = target.request().post(Entity.entity(userInfo, "application/json"));
+        /**
+         * Requête POST à l'api pour ajouter l'utilisateur créé auparavant à
+         * l'application
+         */
+        WebTarget target = client.target(baseUrl).path("users").path("1").queryParam("apiKey", apiKeyWithUsers);
+        Response response = target.request().put(Entity.entity(userInfo, "application/json"));
 
-       /**
-        * Check that we have received a 201 (CREATED) status code and that the
-        * Location header is there.
-        */
-       assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
-       //assertThat(response.getHeaderString("Location")).isNotNull();
+        /**
+         * L'api doit renvoyer un code 201
+         */
+        assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
 
-       /**
-        * Send a GET request to retrieve the entity that was just created. If the
-        * Location header is correct and if the resource has been created, we can
-        * validate its state compared to what we have sent before.
-        */
-       //target = client.target(response.getHeaderString("Location"));
-       //response = target.request().get();
-       String jsonPayload = response.readEntity(String.class);
-       JsonNode root = mapper.readTree(jsonPayload);
-       assertThat(root.get("firstname").asText().compareToIgnoreCase(firstName));
-       assertThat(root.get("lastname").asText().compareToIgnoreCase(lastName));
+        /**
+         * L'api doit renvoyer les informations de l'utilisateur correctes
+         */
+        String jsonPayload = response.readEntity(String.class);
+        JsonNode root = mapper.readTree(jsonPayload);
+        assertThat(root.get("firstname").asText().compareToIgnoreCase(firstName));
+        assertThat(root.get("lastname").asText().compareToIgnoreCase(lastName));
     }
 }
