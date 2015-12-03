e952cf802d026b87d19d43996a48fd2d78000d8d
Thibaud Duchoud <thibduch@hotmail.com>
2015-12-03 10:25:31 +0100

    Update rest api and add Application Demo

--- projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/response/SendApiKey.java
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/response/SendApiKey.java
@@ -19,6 +19,11 @@ public class SendApiKey extends SendResponse {
         return Response.status(Response.Status.UNAUTHORIZED)
                 .entity(new ErrorObject("No API key was provided with the API request"))
                 .type(MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
                 .build();
     }
 
@@ -26,6 +31,11 @@ public class SendApiKey extends SendResponse {
         return Response.status(Response.Status.UNAUTHORIZED)
                 .entity(new ErrorObject("An invalid API key was provided with the API request"))
                 .type(MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
                 .build();
     }
 
--- projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/response/SendResponse.java
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/response/SendResponse.java
@@ -18,20 +18,35 @@ public class SendResponse {
         return Response.status(Response.Status.OK)
                 .entity(toSend)
                 .type(MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
                 .build();
     }
-    
+
     public static Response send201Created(Object toSend) {
-       return Response.status(Response.Status.CREATED)
-               .entity(toSend)
-               .type(MediaType.APPLICATION_JSON)
-               .build();
+        return Response.status(Response.Status.CREATED)
+                .entity(toSend)
+                .type(MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .build();
     }
 
     public static Response missingDataInPayload(Object toSend) {
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(toSend)
                 .type(MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
                 .build();
     }
 
