041db7bb30076c4785e36e3c051cacb27765e0b4
Thibaud Duchoud <thibduch@hotmail.com>
2015-12-04 10:38:29 +0100

    Update app demo

--- projet/ApplicationDemo/routes/index.js
+++ projet/ApplicationDemo/routes/index.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
+
+
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Application Demo' });
+
+      res.render('index', {
+         title: 'Application Demo'
+      });
+
+});
+
+router.get('/manageBadges', function(req, res) {
+
+      res.render('manage-badges', {
+         title: 'Manage badges'
+      });
+
+});
+
+router.get('/manageRewards', function(req, res) {
+
+      res.render('index', {
+         title: 'Application Demo'
+      });
+
+});
+
+router.get('/showLeaderboard', function(req, res) {
+      res.render('leaderboard', {
+         title: 'Leaderboard'
+      });
 });
 
 module.exports = router;
