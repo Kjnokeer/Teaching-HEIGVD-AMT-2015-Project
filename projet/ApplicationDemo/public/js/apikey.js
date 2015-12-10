20771af20c52fd6784081fb0ef8b1579d140e9d9
Thibaud Duchoud <thibduch@hotmail.com>
2015-12-10 20:25:49 +0100

    Update app demo

--- projet/ApplicationDemo/public/js/apikey.js
+++ projet/ApplicationDemo/public/js/apikey.js
@@ -1,3 +1,1 @@
 var API_KEY = "0d969d5771ea42888b70db4fb44f9e67";
-
-module.exports = API_KEY;
--- projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/ApplicationConfig.java
+++ projet/MoussaRaser/src/main/java/ch/heigvd/amt/moussaraser/rest/config/ApplicationConfig.java
@@ -44,7 +44,7 @@ public class ApplicationConfig extends Application {
         resources.add(ch.heigvd.amt.moussaraser.rest.config.MyObjectMapperProvider.class);
         resources.add(ch.heigvd.amt.moussaraser.rest.resources.BadgesResource.class);
         resources.add(ch.heigvd.amt.moussaraser.rest.resources.LeaderBoardRessource.class);
-      resources.add(ch.heigvd.amt.moussaraser.rest.resources.RewardsRessource.class);
+        resources.add(ch.heigvd.amt.moussaraser.rest.resources.RewardsRessource.class);
         resources.add(ch.heigvd.amt.moussaraser.rest.resources.UsersResource.class);
     }
 
