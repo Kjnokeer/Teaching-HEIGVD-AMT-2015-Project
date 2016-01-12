646e15bd92eff760bbfd9906863e6f3d8f84f3ef
Thibaud Duchoud <thibduch@hotmail.com>
2016-01-12 15:15:25 +0100

    App demo DB

--- projet/PublicInsta/DB/db.sql
+++ projet/PublicInsta/DB/db.sql
@@ -1,1 +1,59 @@
- 
+DROP SCHEMA IF EXISTS easygoing;
+CREATE SCHEMA publicInsta;
+
+/* Creation of tables */
+USE publicInsta;
+
+CREATE TABLE user
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   email VARCHAR(64) NOT NULL,
+   password VARCHAR(64) NOT NULL,
+   username VARCHAR(64) NOT NULL,
+   profile_photo VARCHAR(64),
+   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   UNIQUE(email),
+   PRIMARY KEY(id)
+);
+
+
+CREATE TABLE image
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   path VARCHAR(64) NOT NULL,
+   text VARCHAR(256),
+   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE comment
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   text VARCHAR(256) NOT NULL,
+   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+   user_id INT NOT NULL,
+   image_id INT NOT NULL,
+
+   FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,
+   FOREIGN KEY(image_id) REFERENCES image(id) ON DELETE CASCADE,
+
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE opinion
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   positiv INT DEFAULT 0,
+   negativ INT DEFAULT 0,
+   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+   user_id INT NOT NULL,
+   image_id INT NOT NULL,
+
+   FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,
+   FOREIGN KEY(image_id) REFERENCES image(id) ON DELETE CASCADE,
+
+   PRIMARY KEY(id)
+);
