c14df83ee9d37d7d298e64712c9e04b30f85d4de
Thibaud Duchoud <thibduch@hotmail.com>
2016-01-14 15:15:42 +0100

    Update

--- projet/PublicInsta/WEB/infiniteScroll.php
+++ projet/PublicInsta/WEB/infiniteScroll.php
@@ -5,7 +5,13 @@ require_once 'php/pdo.php';
 if( isset( $_POST['start'] ) && isset( $_POST['limit'] ) && !empty( $_POST['start'] ) && !empty( $_POST['limit'] ) ) {
    $start = $_POST['start'];
    $limit = $_POST['limit'];
-   $query = "SELECT username, profile_photo, path, text FROM image INNER JOIN user ON user.id = image.user_id ORDER BY user.id LIMIT $start, $limit";
+   if($start == 1) {
+      $query = "SELECT username, profile_photo, path, text FROM image INNER JOIN user ON user.id = image.user_id ORDER BY user.id LIMIT 0, $limit";
+   }
+   else {
+      $query = "SELECT username, profile_photo, path, text FROM image INNER JOIN user ON user.id = image.user_id ORDER BY user.id LIMIT $start, $limit";
+   }
+   
 
    $sqlp = $GLOBALS["pdo"]->query($query);
    $data['count'] = $sqlp->rowCount();
