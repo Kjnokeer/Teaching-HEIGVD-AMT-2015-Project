e2b1e230cdf4f81764865950101fdd5479a1d437
Mario Ferreira <mario.jorge.ferreira@gmail.com>
2016-01-14 14:59:25 +0100

    Upload image

--- projet/PublicInsta/WEB/content.php
+++ projet/PublicInsta/WEB/content.php
@@ -14,6 +14,9 @@ if(loggedIn() == false) {
 
   <title>PublicInsta</title>
 
+  link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <!--<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Pacifico' type='text/css'>-->
@@ -52,6 +55,28 @@ if(loggedIn() == false) {
   </nav>
 
   <div class="container content">
+
+    <div class="picture_post">
+      <div class="post_header">
+        <div class="picture_post_header">
+          <img class="avatar img-circle" src="<?php echo $_SESSION['profile_photo']; ?>" />
+        </div>
+        <div class="username_post_header">
+          <a><?php echo $_SESSION['username']; ?></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="post_content col-md-12">
+          <div class="form-group">
+            <input type="text" name="photo_caption" id="photo_caption" tabindex="1" class="form-control" placeholder="Message...">
+          </div>
+          <div class="form-group">
+            <input id="photo_upload" type="file" tabindex="2" class="file file-loading" accept="image/*" >
+          </div>
+
+        </div>
+      </div>
+    </div>
 
     <?php
     $sql = 'SELECT username, profile_photo, path, text
@@ -112,5 +137,9 @@ if(loggedIn() == false) {
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
+  <!-- canvas-to-blob.min.js is only needed if you wish to resize images before upload.
+  This must be loaded before fileinput.min.js -->
+  <script src="js/plugins/canvas-to-blob.min.js" type="text/javascript"></script>
+  <script src="js/fileinput.min.js"></script>
 </body>
 </html>
