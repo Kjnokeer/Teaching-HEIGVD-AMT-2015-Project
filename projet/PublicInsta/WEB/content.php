491eea12081a2f26e0c4b6921d23664d9c0f95c1
Thibaud Duchoud <thibduch@hotmail.com>
2016-01-15 09:42:50 +0100

    Add partials

--- projet/PublicInsta/WEB/content.php
+++ projet/PublicInsta/WEB/content.php
@@ -37,33 +37,8 @@ define('LIMIT', 4);
   <![endif]-->
 </head>
 <body>
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="content.php">PublicInsta</a>
-      </div>
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="content.php">News</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="user_posts.php">Posts</a></li>
-              <li><a href="awards.php">Awards</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="logout.php">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div>
-  </nav>
+  
+  <?php include('partials/navbar.php'); ?>
 
   <div class="container content" id="content">
 
